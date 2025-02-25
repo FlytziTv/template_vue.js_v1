c7a636ca03ba7088747d5021da54dc2c49b05b54
FlytziTv <flytzitv@gmail.com>
2025-02-25 16:31:29 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'boxicons/css/boxicons.min.css';
+import '@fontsource/inter';
+
+createApp(App).mount('#app')
